effec8b11ad464f1be25df862aa73d3a9f58ef72
EllisonCzp <2283487595@qq.com>
2019-07-18 16:14:15 +0800

    修改了main.js

--- src/main.js
+++ src/main.js
@@ -1,19 +1,19 @@
 //入口文件
-import Vue from 'vue'
+import Vue from 'vue';
 
 
 // 导入mui 样式
-import './lib/mui/css/mui.css'
+import './lib/mui/css/mui.css';
 
 // 按需导入 mint-ui 中的组件
-import { Header } from 'mint-ui'
+import { Header } from 'mint-ui';
 Vue.component(Header.name, Header);
 
 // 导入 App 根组件
-import app from './App.vue'
+import app from './App.vue';
 
 
 var vm = new Vue({
     el: '#app',
     render: c => c(app)
-})
+});
